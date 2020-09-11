403a4973194e157717b2e8ac71b802c3410c8a67
ktnxd <ktnxbaii@gmail.com>
2020-09-11 20:59:49 +0100

    spelling

--- Arch.sh
+++ Arch.sh
@@ -12,9 +12,9 @@ cp -r Archstuffs/.config/polybar ~/.config/ ;
 cp -r Archstuffs/.config/sxhkd ~/.config/ ; 
 cp -r Archstuffs/.config/termite ~/.config/ ;
 cp -r Archstuffs/.config/xfce4 ~/.config/ ;
-echo Istalling Useful Shit
+echo Installing Useful Shit
 sleep 1 ;	
-sudo pacman -Syu cmake libsystemd qt5-base zlib libappindicator-gtk2 quazip libpulse qt5-tools libxcb xcb-util-wm qt5-x11extras git filelight nvidia nvidia-cg-toolkit nvidia-dkms nvidia-prime nvidia-settings nvidia-utils nvidia-utils-openrc wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader nvidia-utils lib32-nvidia-utils firefox discord steam-native-runtime multilib-devel vkd3d lib32-vkd3d repo lutris cowsay ;
+sudo pacman -Syu -y lib32-gcc-libs cmake libsystemd qt5-base zlib libappindicator-gtk2 quazip libpulse qt5-tools libxcb xcb-util-wm qt5-x11extras git filelight nvidia nvidia-cg-toolkit nvidia-dkms nvidia-prime nvidia-settings nvidia-utils nvidia-utils-openrc wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader nvidia-utils lib32-nvidia-utils firefox discord steam-native-runtime multilib-devel vkd3d lib32-vkd3d repo lutris cowsay ;
 echo Keyboard Drivers ;
 sleep 1 ;
 git clone https://github.com/ckb-next/ckb-next.git ;
@@ -28,7 +28,8 @@ cd yay/
 makepkg -si ;
 cd ;
 yay -S gradle maven protontricks gamemode matcha-gtk-theme qogir-icon-theme ;
-git clone https://github.com/akhilnarang/scripts ; 
+echo AOSP devel
+git clone https://github.com/akhilnarang/scripts ;
 cd scripts ;
 bash setup/arch-manjaro.sh ;
 cd ;
