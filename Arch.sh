4ab63fb419437fec2b763fdfd4d2b93e84c4a3a2
ktnxd <ktnxbaii@gmail.com>
2020-09-12 00:39:03 +0100

    finalising

--- Arch.sh
+++ Arch.sh
@@ -14,25 +14,28 @@ cp -r Archstuffs/.config/termite ~/.config/ ;
 cp -r Archstuffs/.config/xfce4 ~/.config/ ;
 echo Installing Useful Shit
 sleep 1 ;	
-sudo pacman -Syu -y lib32-gcc-libs cmake libsystemd qt5-base zlib libappindicator-gtk2 quazip libpulse qt5-tools libxcb xcb-util-wm qt5-x11extras git filelight nvidia nvidia-cg-toolkit nvidia-dkms nvidia-prime nvidia-settings nvidia-utils nvidia-utils-openrc wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader nvidia-utils lib32-nvidia-utils firefox discord steam-native-runtime multilib-devel vkd3d lib32-vkd3d repo lutris cowsay ;
+sudo pacman -Syu lib32-gcc-libs cmake libsystemd qt5-base zlib libappindicator-gtk2 quazip libpulse qt5-tools libxcb xcb-util-wm qt5-x11extras git filelight nvidia nvidia-cg-toolkit nvidia-dkms nvidia-prime nvidia-settings nvidia-utils nvidia-utils-openrc wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader nvidia-utils lib32-nvidia-utils firefox discord steam-native-runtime multilib-devel vkd3d lib32-vkd3d repo lutris cowsay piper ;
 echo Keyboard Drivers ;
 sleep 1 ;
 git clone https://github.com/ckb-next/ckb-next.git ;
 cd ckb-next/ ;
 ./quickinstall ;
 cd ;
-echo YAY ; 
-sleep 1 ;
-git clone https://aur.archlinux.org/yay.git ;
-cd yay/
-makepkg -si ;
-cd ;
-yay -S gradle maven protontricks gamemode matcha-gtk-theme qogir-icon-theme ;
-echo AOSP devel
-git clone https://github.com/akhilnarang/scripts ;
-cd scripts ;
-bash setup/arch-manjaro.sh ;
-cd ;
+# Install pacaur
+echo AUR
+sudo pacman -S base-devel git wget multilib-devel cmake svn clang lzip patchelf inetutils python2-distlib
+# Install ncurses5-compat-libs, lib32-ncurses5-compat-libs, aosp-devel, xml2, and lineageos-devel
+for package in ncurses5-compat-libs lib32-ncurses5-compat-libs aosp-devel xml2 lineageos-devel; do
+    git clone https://aur.archlinux.org/"${package}"
+    cd "${package}" || continue
+    makepkg -si --skippgpcheck
+    cd - || break
+    rm -rf "${package}"
+done
+
+echo -e "Installing platform tools & udev rules for adb!"
+sudo pacman -S android-tools android-udev ;
+yay -S gradle maven protontricks gamemode matcha-gtk-theme qogir-icon-theme 
 git config --global user.email ktnxbaii@gmail.com ; git config --global user.name ktnxd ;
 rm -rf ckb-next yay ;
 mkdir proton ;
