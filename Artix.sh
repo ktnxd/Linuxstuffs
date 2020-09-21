222ecf46f736d12ed95fdcc326d6bf9f08a33e8a
ktnxd <ktnxbaii@gmail.com>
2020-09-21 22:32:16 +0100

    fixed previous bork

--- Artix.sh
+++ Artix.sh
@@ -2,28 +2,27 @@
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf ;
 echo Moving Personal Configs ;
 sleep 1 ;
-cp Archstuffs/.bashrc ~/.bashrc ;
-cp Archstuffs/wallhaven-4l5jkp.jpg ~/Pictures ; 
-cp -r Archstuffs/.Xresources ~/.Xresources ; 
-cp -r Archstuffs/.config/bspwm ~/.config/ ;
-cp -r Archstuffs/.config/dunst ~/.config/ ;
-cp -r Archstuffs/.config/mpd ~/.config/ ;
-cp -r Archstuffs/.config/polybar ~/.config/ ;
-cp -r Archstuffs/.config/sxhkd ~/.config/ ; 
-cp -r Archstuffs/.config/termite ~/.config/ ;
-cp -r Archstuffs/.config/xfce4 ~/.config/ ;
+cd ;
+cp Linuxstuffs/.bashrc ~/.bashrc ;
+cp Linuxstuffs/wallhaven-4l5jkp.jpg ~/Pictures ; 
+cp -r Linuxstuffs/.Xresources ~/.Xresources ; 
+cp -r Linuxstuffs/.config/bspwm ~/.config/ ;
+cp -r Linuxstuffs/.config/dunst ~/.config/ ;
+cp -r Linuxstuffs/.config/mpd ~/.config/ ;
+cp -r Linuxstuffs/.config/polybar ~/.config/ ;
+cp -r Linuxstuffs/.config/sxhkd ~/.config/ ; 
+cp -r Linuxstuffs/.config/termite ~/.config/ ;
+cp -r Linuxstuffs/.config/xfce4 ~/.config/ ;
 echo Installing Useful Shit
 sleep 1 ;	
 sudo pacman -Syu lib32-gcc-libs cmake libsystemd qt5-base zlib libappindicator-gtk2 quazip libpulse qt5-tools libxcb xcb-util-wm qt5-x11extras git filelight nvidia nvidia-cg-toolkit nvidia-dkms nvidia-prime nvidia-settings nvidia-utils nvidia-utils-openrc wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader nvidia-utils lib32-nvidia-utils firefox discord steam-native-runtime exa multilib-devel vkd3d lib32-vkd3d repo lutris cowsay piper noto-fonts noto-fonts-emoji ;
+# ckb-next
 echo Keyboard Drivers ;
 sleep 1 ;
 git clone https://github.com/ckb-next/ckb-next.git ;
 cd ckb-next/ ;
 ./quickinstall ;
 cd ;
-git clone https://aur.archlinux.org/yay.git ;
-cd yay ;
-makepkg -si ;
 # Install pacaur
 echo AUR
 sudo pacman -S base-devel git wget multilib-devel cmake svn clang lzip patchelf inetutils python2-distlib
@@ -38,19 +37,28 @@ done
 
 echo -e "Installing platform tools & udev rules for adb!"
 sudo pacman -S android-tools android-udev ;
+echo -e "Installing more useful shit" ; 
 yay -S gradle maven protontricks gamemode matcha-gtk-theme qogir-icon-theme 
 git config --global user.email ktnxbaii@gmail.com ; git config --global user.name ktnxd ;
 rm -rf ckb-next yay ;
 mkdir proton ;
 cowsay -f tux all done nigga ;
-echo rebooting in 5
+sleep 2
+clear
+cowsay -f tux Rebooting in 5
 sleep 1
-echo		  4
+clear
+cowsay -f tux Rebooting in 4
 sleep 1
-echo		  3
+clear
+cowsay -f tux Rebooting in 3
 sleep 1
-echo		  2
+clear
+cowsay -f tux Rebooting in 2
 sleep 1
-echo		  1
-echo sudo rm -rf --no-preserve-root
+clear
+cowsay -f tux Rebooting in 1
+sleep 1
+clear
+cowsay -f sudo rm -rf / --no-preserve-root
 sudo reboot
