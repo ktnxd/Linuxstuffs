9dd66e7d9bc36dee744fa46b0f94f356745df05d
ktnxd <ktnxbaii@gmail.com>
2020-09-24 00:26:12 +0100

    spelling

--- Artixpad.sh
+++ Artixpad.sh
@@ -1,7 +1,8 @@
 # Uncomment the multilib repo, incase it was commented out
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf ;
+sudo pacman -Syyyu
 echo Moving Personal Configs ;
-sleep 1 ;
+sleep 3 ;
 cp Linuxstuffs/.bashrc ~/.bashrcTP ;
 cp Linuxstuffs/wallhaven-4l5jkp.jpg ~/Pictures ; 
 cp -r Linuxstuffs/.Xresources ~/.Xresources ; 
@@ -13,20 +14,12 @@ cp -r Linuxstuffs/.config/sxhkd ~/.config/ ;
 cp -r Linuxstuffs/.config/termite ~/.config/ ;
 cp -r Linuxstuffs/.config/xfce4 ~/.config/ ;
 echo Installing Useful Shit
-sleep 1 ;	
-sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-intel intel-ucode lib32-vulkan-intel xf86-video-intel intel-mediai-driver  vulkan-icd-loader lib32-vulkan-icd-loader steam-native-runtime discord firefox multilib-devel exa repo git lutris cowsay notofonts noto-fonts-emoji termite qbittorrent lolcat vlc ;
-sleep 1 ;
-clear ;
-# yay
-echo Installing yay ;
-sleep 1
-git clone https://aur.archlinux.org/yay.git ;
-cd yay ;
-makepkg -si ;
-cd ;
-clear ;
+sleep 3 ;	
+sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-intel intel-ucode lib32-vulkan-intel xf86-video-intel intel-media-driver  vulkan-icd-loader lib32-vulkan-icd-loader steam-native-runtime discord firefox multilib-devel exa repo git lutris cowsay noto-fonts noto-fonts-emoji termite qbittorrent lolcat vlc ;
+sleep 3 ;
 # AOSP stuff
 echo AOSP stuff ;
+sleep 3 ;
 sudo pacman -S base-devel git wget multilib-devel cmake svn clang lzip patchelf inetutils python2-distlib
 # Install ncurses5-compat-libs, lib32-ncurses5-compat-libs, aosp-devel, xml2, and lineageos-devel
 for package in ncurses5-compat-libs lib32-ncurses5-compat-libs aosp-devel xml2 lineageos-devel; do
@@ -36,10 +29,15 @@ for package in ncurses5-compat-libs lib32-ncurses5-compat-libs aosp-devel xml2 l
     cd - || break
     rm -rf "${package}"
 done
-clear ;
 echo -e "Installing platform tools & udev rules for adb!"
 sudo pacman -S android-tools android-udev ;
-clear ;
+cd ;
+echo AUR packages ;
+sleep 3 ;
+git clone https://aur.archlinux.org/yay.git ;
+cd yay/ ;
+makepkg -si ;
+cd ;
 yay -S gradle maven protontricks gamemode matcha-gtk-theme qogir-icon-theme 
 git config --global user.email ktnxbaii@gmail.com ; git config --global user.name ktnxd ;
 rm -rf ckb-next yay ;
