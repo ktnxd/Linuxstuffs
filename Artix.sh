5408873fb4c89dee822d6b66ba3028bfafab80e6
ktnxd <ktnxbaii@gmail.com>
2020-09-23 22:08:34 +0100

    tor

--- Artix.sh
+++ Artix.sh
@@ -15,7 +15,7 @@ cp -r Linuxstuffs/.config/termite ~/.config/ ;
 cp -r Linuxstuffs/.config/xfce4 ~/.config/ ;
 echo Installing Useful Shit
 sleep 1 ;	
-sudo pacman -Syu lib32-gcc-libs cmake libsystemd qt5-base zlib libappindicator-gtk2 quazip libpulse qt5-tools libxcb xcb-util-wm qt5-x11extras git filelight nvidia nvidia-cg-toolkit nvidia-dkms nvidia-prime nvidia-settings nvidia-utils nvidia-utils-openrc wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader nvidia-utils lib32-nvidia-utils firefox discord steam-native-runtime exa multilib-devel vkd3d lib32-vkd3d repo lutris cowsay piper noto-fonts noto-fonts-emoji feh ;
+sudo pacman -Syu lib32-gcc-libs cmake libsystemd qt5-base zlib libappindicator-gtk2 quazip libpulse qt5-tools libxcb xcb-util-wm qt5-x11extras git filelight nvidia nvidia-cg-toolkit nvidia-dkms nvidia-prime nvidia-settings nvidia-utils nvidia-utils-openrc wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader nvidia-utils lib32-nvidia-utils firefox discord steam-native-runtime exa multilib-devel vkd3d lib32-vkd3d repo lutris cowsay piper noto-fonts noto-fonts-emoji feh tor torbrowser-launcher ;
 # ckb-next
 echo Keyboard Drivers ;
 sleep 1 ;
@@ -23,8 +23,8 @@ git clone https://github.com/ckb-next/ckb-next.git ;
 cd ckb-next/ ;
 ./quickinstall ;
 cd ;
-# AUR
-echo AUR ;
+# yay
+echo Installing yay ;
 sleep 1 ;
 git clone https://aur.archlinux.org/yay.git ;
 cd yay ;
@@ -45,7 +45,7 @@ done
 echo -e "Installing platform tools & udev rules for adb!"
 sudo pacman -S android-tools android-udev ;
 echo -e "Installing more useful shit" ; 
-yay -S gradle maven protontricks gamemode matcha-gtk-theme qogir-icon-theme 
+yay -S gradle maven protontricks gamemode matcha-gtk-theme qogir-icon-theme etcher ; 
 git config --global user.email ktnxbaii@gmail.com ; git config --global user.name ktnxd ;
 rm -rf ckb-next yay ;
 mkdir proton ;
--- Artixpad.sh
+++ Artixpad.sh
@@ -2,8 +2,7 @@
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf ;
 echo Moving Personal Configs ;
 sleep 1 ;
-rm -rf .bashrc
-cp Linuxstuffs/.bashrcTP ~/.bashrc ;
+cp Linuxstuffs/.bashrc ~/.bashrcTP ;
 cp Linuxstuffs/wallhaven-4l5jkp.jpg ~/Pictures ; 
 cp -r Linuxstuffs/.Xresources ~/.Xresources ; 
 cp -r Linuxstuffs/.config/bspwm ~/.config/ ;
@@ -15,13 +14,19 @@ cp -r Linuxstuffs/.config/termite ~/.config/ ;
 cp -r Linuxstuffs/.config/xfce4 ~/.config/ ;
 echo Installing Useful Shit
 sleep 1 ;	
-sudo pacman -S discord firefox multilib-devel exa repo git lutris cowsay noto-fonts noto-fonts-emoji termite qbittorrent lolcat vlc tor torbrowser-launcher ;
+sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader lib32-mesa vulkan-intel intel-ucode lib32-vulkan-intel xf86-video-intel intel-mediai-driver  vulkan-icd-loader lib32-vulkan-icd-loader steam-native-runtime discord firefox multilib-devel exa repo git lutris cowsay notofonts noto-fonts-emoji termite qbittorrent lolcat vlc ;
+sleep 1 ;
+clear ;
+# yay
+echo Installing yay ;
+sleep 1
 git clone https://aur.archlinux.org/yay.git ;
 cd yay ;
 makepkg -si ;
 cd ;
-echo installing pacaur ;
-sleep 1 ;
+clear ;
+# AOSP stuff
+echo AOSP stuff ;
 sudo pacman -S base-devel git wget multilib-devel cmake svn clang lzip patchelf inetutils python2-distlib
 # Install ncurses5-compat-libs, lib32-ncurses5-compat-libs, aosp-devel, xml2, and lineageos-devel
 for package in ncurses5-compat-libs lib32-ncurses5-compat-libs aosp-devel xml2 lineageos-devel; do
@@ -31,31 +36,24 @@ for package in ncurses5-compat-libs lib32-ncurses5-compat-libs aosp-devel xml2 l
     cd - || break
     rm -rf "${package}"
 done
-
+clear ;
 echo -e "Installing platform tools & udev rules for adb!"
 sudo pacman -S android-tools android-udev ;
+clear ;
 yay -S gradle maven protontricks gamemode matcha-gtk-theme qogir-icon-theme 
 git config --global user.email ktnxbaii@gmail.com ; git config --global user.name ktnxd ;
 rm -rf ckb-next yay ;
 mkdir proton ;
 yay -Sc ;
 cowsay -f tux all done nigga ;
-sleep 2
-clear
-cowsay -f tux Rebooting in 5
+echo rebooting in 5
 sleep 1
-clear
-cowsay -f tux Rebooting in 4
+echo		  4
 sleep 1
-clear
-cowsay -f tux Rebooting in 3
+echo		  3
 sleep 1
-clear
-cowsay -f tux Rebooting in 2
+echo		  2
 sleep 1
-clear
-cowsay -f tux Rebooting in 1
-sleep 1
-clear
-cowsay -f sudo rm -rf / --no-preserve-root
+echo		  1
+echo sudo rm -rf --no-preserve-root
 sudo reboot
