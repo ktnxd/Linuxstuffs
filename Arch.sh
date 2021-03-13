c3b2eaded1ea1a227650d7334abce0e36ddaf21b
ktnxd <ktnxbaii@gmail.com>
2021-03-13 22:37:16 +0000

    fixes

--- Arch.sh
+++ Arch.sh
@@ -2,55 +2,45 @@
 sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf ;
 echo Moving Personal Configs ;
 sleep 1 ;
-cp Linuxstuffs/.bashrc2 ~/.bashrc ;
-cp Linuxtuffs/wallhaven-4l5jkp.jpg ~/Pictures ; 
-cp -r Linuxstuffs/.Xresources ~/.Xresources ; 
-cp -r Linuxstuffs/.config/bspwm ~/.config/ ;
-cp -r Linuxstuffs/.config/dunst ~/.config/ ;
-cp -r Linuxstuffs/.config/mpd ~/.config/ ;
-cp -r Linuxstuffs/.config/polybar ~/.config/ ;
-cp -r Linuxstuffs/.config/sxhkd ~/.config/ ; 
-cp -r Linuxstuffs/.config/termite ~/.config/ ;
+cp Linuxstuffs/.bashrc ~/.bashrc ;
+cp Linuxtuffs/wallhaven.jpg ~/Pictures ; 
+#cp -r Linuxstuffs/.Xresources ~/.Xresources ; 
+#cp -r Linuxstuffs/.config/bspwm ~/.config/ ;
+#cp -r Linuxstuffs/.config/dunst ~/.config/ ;
+#cp -r Linuxstuffs/.config/mpd ~/.config/ ;
+#cp -r Linuxstuffs/.config/polybar ~/.config/ ;
+#cp -r Linuxstuffs/.config/sxhkd ~/.config/ ; 
+#cp -r Linuxstuffs/.config/termite ~/.config/ ;
 cp -r Linuxstuffs/.config/xfce4 ~/.config/ ;
-echo Installing Useful Shit
+echo Installing Useful Shit ;
 sleep 1 ;	
-sudo pacman -Syu lib32-gcc-libs cmake libsystemd qt5-base zlib libappindicator-gtk2 quazip libpulse qt5-tools libxcb xcb-util-wm qt5-x11extras git filelight nvidia nvidia-cg-toolkit nvidia-dkms nvidia-prime nvidia-settings nvidia-utils wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader nvidia-utils lib32-nvidia-utils firefox discord steam-native-runtime multilib-devel vkd3d lib32-vkd3d repo lutris cowsay piper noto-fonts noto-fonts-emoji exa ;
-echo Keyboard Drivers ;
+sudo pacman -Syu xfce4 xfce4-goodies xorg lib32-gcc-libs cmake libsystemd qt5-base zlib libappindicator-gtk2 quazip libpulse qt5-tools libxcb xcb-util-wm qt5-x11extras git filelight nvidia nvidia-cg-toolkit nvidia-dkms nvidia-prime nvidia-settings nvidia-utils wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader nvidia-utils lib32-nvidia-utils discord steam-native-runtime multilib-devel vkd3d lib32-vkd3d repo lutris noto-fonts noto-fonts-emoji exa cowsay pulseaudio pavucontrol ttf-ubuntu-font-family ;
+echo Installing Paru ;
 sleep 1 ;
-git clone https://github.com/ckb-next/ckb-next.git ;
-cd ckb-next/ ;
-./quickinstall ;
-cd ;
-git clone https://aur.archlinux.org/yay.git ;
-cd yay ;
+git clone https://aur.archlinux.org/paru.git ;
+cd paru ;
 makepkg -si ;
-# Install pacaur
-echo AUR
-sudo pacman -S base-devel git wget multilib-devel cmake svn clang lzip patchelf inetutils python2-distlib
-# Install ncurses5-compat-libs, lib32-ncurses5-compat-libs, aosp-devel, xml2, and lineageos-devel
-for package in ncurses5-compat-libs lib32-ncurses5-compat-libs aosp-devel xml2 lineageos-devel; do
-    git clone https://aur.archlinux.org/"${package}"
-    cd "${package}" || continue
-    makepkg -si --skippgpcheck
-    cd - || break
-    rm -rf "${package}"
-done
-
-echo -e "Installing platform tools & udev rules for adb!"
+cd ;
+echo -e "Installing platform tools & udev rules for adb!" ;
 sudo pacman -S android-tools android-udev ;
-yay -S gradle maven protontricks gamemode matcha-gtk-theme qogir-icon-theme 
+paru -S gradle maven protontricks gamemode matcha-gtk-theme qogir-icon-theme librewolf-bin ; 
 git config --global user.email ktnxbaii@gmail.com ; git config --global user.name ktnxd ;
-rm -rf ckb-next yay ;
 mkdir proton ;
 cowsay -f tux all done nigga ;
-echo rebooting in 5
-sleep 1
-echo		  4
-sleep 1
-echo		  3
-sleep 1
-echo		  2
-sleep 1
-echo		  1
-echo sudo rm -rf --no-preserve-root
+echo rebooting in 5 ;
+sleep 1 ;
+cowsay -f tux 4 ;
+sleep 1 ;
+clear ;
+cowsay -f tux 3 ;
+sleep 1 ;
+clear ;
+cowsay -f tux 2 ;
+sleep 1 ;
+clear ;
+cowsay -f tux 1 ;
+sleep 1 ;
+clear ;
+cowsay -f tux byebye ;
+echo sudo rm -rf --no-preserve-root ;
 sudo reboot
