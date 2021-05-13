7d4d15786f291b1fb2456d3daaf4823ee2679cb5
ktnxd <ktnxbaii@gmail.com>
2021-05-14 00:00:26 +0100

    rm package

--- Artixpadgnome.sh
+++ Artixpadgnome.sh
@@ -3,12 +3,12 @@ sudo sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf ;
 echo Moving Personal Configs ;
 sleep 1;
 cp Linuxstuffs/.bashrctp ~/.bashrc ;
-sudo cp Linuxstuffs/.bashArtixROOT /root/.bashrc ;
+sudo cp Linuxstuffs/.bashrcArtixROOT /root/.bashrc ;
 cp Linuxstuffs/wallhaven.jpg ~/Pictures ; 
 #cp -r Linuxstuffs/.config-thinkpad/xfce4 ~/.config/ ;
 echo Stuffs ;
 sleep 3 ;
-sudo pacman -Syu --noconfirm discord firefox xorg xorg-xinit cowsay exa noto-fonts noto-fonts-emoji qbittorrent gamemode vlc repo libappindicator-gtk2 libappindicator-gtk3 libappletdecoration lib32-libappindicator-gtk3 lib32-libappindicator-gtk2 gnome-shell-extension-appindicator ;
+sudo pacman -Syu --noconfirm discord firefox xorg xorg-xinit cowsay exa noto-fonts noto-fonts-emoji qbittorrent gamemode vlc repo libappindicator-gtk2 libappindicator-gtk3 lib32-libappindicator-gtk3 lib32-libappindicator-gtk2 gnome-shell-extension-appindicator ;
 echo Wine ;
 sudo pacman -S --noconfirm wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader cowsay lolcat &&
 echo Drivers ;
