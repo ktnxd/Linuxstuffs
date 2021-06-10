1d95f85bfeb8a85a2e5da3666b48236589aff9e5
ktnxd <ktnxbaii@gmail.com>
2021-06-10 13:38:13 +0100

    new fix for Artix

---
+++ Artixnew.sh
@@ -0,0 +1,24 @@
+echo Enable Arch Repo ;
+sudo pacman -Syyy artix-archlinux-support ;
+sudo echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf ;
+sudo echo -e "\n[extra]\nInclude = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf ;
+sudo echo -e "\n[community]\nInclude = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf ;
+sudo echo -e "\n[universe]\nServer = https://universe.artixlinux.org/$arch ;
+sudo sed -i "/\[lib32\]/,/Include/"'s/^#//' /etc/pacman.conf" ;
+sudo pacman-key --populate archlinux
+cd ;
+echo installing shit ;
+sudo pacman -Syu --noconfirm base-devel git wget multilib-devel cmake svn clang lzip patchelf inetutils openrc-bash-completions bash-completion lib32-gcc-libs cmake libsystemd qt5-base zlib libappindicator-gtk2 quazip libpulse qt5-tools libxcb xcb-util-wm qt5-x11extras git filelight nvidia nvidia-cg-toolkit nvidia-dkms nvidia-prime nvidia-settings nvidia-utils nvidia-utils-openrc wine-staging winetricks giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader nvidia-utils lib32-nvidia-utils firefox discord steam-native-runtime exa vkd3d lib32-vkd3d repo lutris noto-fonts noto-fonts-emoji tor torbrowser-launcher android-tools android-udev papirus-icon-theme &&
+echo ckb
+git clone https://github.com/ckb-next/ckb-next.git ;
+cd ckb-next/ ;
+./quickinstall ;
+cd ;
+echo yay ;
+git clone https://aur.archlinux.org/yay.git ;
+cd yay/ ; 
+makepkg -si ;
+cd ;
+yay -S protontricks gamemode matcha-gtk-theme qogir-gtk-theme qogir-icon-theme etcher
+mkdir proton ;
+echo all done nib
