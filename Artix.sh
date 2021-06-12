9963899c166f05fe8afff900c4a968255e201fe7
ktnxd <ktnxbaii@gmail.com>
2021-06-12 13:54:38 +0100

    stuffs

--- Artix.sh
+++ Artix.sh
@@ -2,12 +2,12 @@ echo Moving configs ;
 cp -r Linuxstuffs/.config/xfce4 ~/.config/ ;
 cp -r Linuxstuffs/.config/neofetch ~/.config/ ;
 cp -r Linuxstuffs/.bashrc ~/.bashrc ;
-sudo cp -rLinuxstuffs/.bashrcArtixROOT /root/.bashrc ;
+sudo cp -r Linuxstuffs/.bashrcArtixROOT /root/.bashrc ;
 cp Linuxstuffs/wallhaven.jpg ~/Pictures ;
-echo installing tools ;
-sudo pacman -Syu base-devel cmake libsystemd qt5-base zlib libdbusmenu-qt5 quazip libpulse qt5-tools libxcb xcb-util-wm qt5-x11extras git multilib-devel make cmake python2-distlib inetutils patchelf clang lzip svn wget discord firefox xorg exa noto-fonts noto-fonts-emoji qbittorrent gamemode lib32-gamemode vlc android-tools android-udev gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-google ;
+echo installing misc ;
+sudo pacman -Syu base-devel vim cmake libsystemd qt5-base zlib libdbusmenu-qt5 quazip libpulse qt5-tools libxcb xcb-util-wm qt5-x11extras git multilib-devel make cmake python2-distlib inetutils patchelf clang lzip svn wget discord firefox xorg exa noto-fonts noto-fonts-emoji qbittorrent gamemode lib32-gamemode vlc android-tools android-udev gvfs-afc gvfs-gphoto2 gvfs-mtp gvfs-google linux-zen linux-zen-headers ;
 echo installing drivers ;
-sudo pacman -S nvidia nvidia-dkms nvidia-utils nvidia-utils-openrc lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader intel-ucode ;
+sudo pacman -S nvidia nvidia-dkms nvidia-utils nvidia-utils-openrc lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader vkd3d intel-ucode ;
 echo installing wine ;
 sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader ;
 echo Paru ;
@@ -19,5 +19,6 @@ paru -Syyy protontricks qogir-icon-theme matcha-gtk-theme qogir-gtk-theme etcher
 git clone https://github.com/ckb-next/ckb-next.git ;
 cd ckb-next ;
 ./quickinstall ;
-cd ; 
+cd ;
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 echo End ;
