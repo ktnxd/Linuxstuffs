4fec863853ad9435aab9a545180bb253db83b6ce
ktnxd <ktnxbaii@gmail.com>
2021-06-12 15:01:02 +0100

    arch

--- Archnew.sh
+++ Archnew.sh
@@ -10,7 +10,7 @@ sudo pacman -Syyyu base-devel multilib-devel libsystemd qt5-base zlib libdbusmen
 #Wine
 sudo pacman -S wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader vkd3d lib32-vkd3d ;
 #Other shit
-sudo pacman -S discord firefox steam steam-native-runtime lutris exa android-tools android-udev papirus-icon-theme tar ;
+sudo pacman -S discord firefox steam steam-native-runtime lutris exa android-tools android-udev papirus-icon-theme tar vlc lightdm-gtk-greeter-settings grub-customizer ;
 #Drivers
 sudo pacman -S nvidia nvidia-utils nvidia-dkms nvidia-cg-toolkit nvidia-prime nvidia-settings lib32-nvidia-utils intel-ucode piper ;
 #BFS
@@ -24,6 +24,8 @@ makepkg -si ;
 cd ;
 #AUR
 paru -S protontricks gamemode matcha-gtk-theme qogir-icon-theme qogir-gtk-theme etcher ;
+#Zen kernel
+sudo pacman -S linux-zen linux-zen-headers && sudo grub-mkconfig -o /boot/grub/grub.cfg
 #Proton
 mkdir GE ; cd GE/ ; wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/6.10-GE-1/Proton-6.10-GE-1.tar.gz ; mkdir -p ~/.steam/root/compatibilitytools.d ; tar -xf Proton-6.10-GE-1.tar.gz -C ~/.steam/root/compatibilitytools.d/ ; cd ;
 mkdir proton ;
