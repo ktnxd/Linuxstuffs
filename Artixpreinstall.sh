da76d80b6a1418893904ee7c33503e397b6f930c
ktnxd <ktnxbaii@gmail.com>
2021-06-16 14:16:06 +0100

    fix borked spelling

--- Artixpreinstall.sh
+++ Artixpreinstall.sh
@@ -2,6 +2,6 @@ echo installing Arch Repos ;
 sudo pacman -Syu artix-archlinux-support ;
 sudo echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf ;
 sudo echo -e "\n[extra]\nInclude = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf ;
-sudo echo -e "\n[comminuty]\nInclude = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf ;
+sudo echo -e "\n[community]\nInclude = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf ;
 sudo pacman -Syyy ;
 
