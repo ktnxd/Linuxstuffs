bf157458f81c7fdfc3553b22bc5721b36ae99221
ktnxd <67026350+ktnxd@users.noreply.github.com>
2021-11-23 23:35:37 +0000

    Add files via upload

--- Artixpad.sh
+++ Artixpad.sh
@@ -4,7 +4,9 @@ sudo cp Linuxstuffs/.bashrcroot /root/.bashrc
 cp Linuxstuffs/wallhaven.jpg ~/Pictures/wallhaven.jpg ;
 cp -r Linuxstuffs/.config/neofetch ~/.config/ ;
 #Base
-sudo pacman -Syyyu base-devel multilib-devel make cmake git &&
+sudo pacman -Syyyu base-devel multilib-devel make cmake git rust &&
+cargo install exa &&
+sudo mv .cargo/bin/exa /bin &&
 #Drivers
 sudo pacman -S --needed intel-ucode xf86-video-intel lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader &&
 #shit
@@ -12,5 +14,5 @@ sudo pacman -S gamemode lib32-gamemode gvfs-mtp fuse transmission-gtk mpv bash-c
 #paru
 git clone https://aur.archlinux.org/paru.git && cd paru/ && makepkg -si && cd &&
 #Other shit
-paru -Syyy brave-bin matcha-gtk-theme-git protontricks-git librewolf-bin lutris exa papirus-icon-theme-git android-udev-git android-sdk-platform-tools android-bash-completion &&
+paru -Syyy brave-bin qogir-icon-theme-git matcha-gtk-theme-git protontricks-git librewolf-bin lutris papirus-icon-theme-git android-udev-git android-sdk-platform-tools android-bash-completion &&
 echo fin
