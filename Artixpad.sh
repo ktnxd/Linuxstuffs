a379357d7cee05ff793b22f87b87c718d7184884
ktnxd <67026350+ktnxd@users.noreply.github.com>
2023-03-12 01:44:05 +0000

    Update Artixpad.sh

--- Artixpad.sh
+++ Artixpad.sh
@@ -4,13 +4,9 @@ sudo cp Linuxstuffs/.bashrcroot /root/.bashrc
 cp Linuxstuffs/wallhaven.jpg ~/Pictures/wallhaven.jpg ;
 cp -r Linuxstuffs/.config/neofetch ~/.config/ ;
 #Base
-sudo pacman -Syyyu base-devel multilib-devel make cmake git rust &&
+sudo pacman -Syyyu base-devel multilib-devel make cmake git rust intel-ucode xf86-video-intel lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader gvfs-mtp fuse transmission-gtk mpv bash-completion openrc-bash-completions &&
 cargo install exa &&
 sudo mv .cargo/bin/exa /bin &&
-#Drivers
-sudo pacman -S --needed intel-ucode xf86-video-intel lib32-mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader &&
-#shit
-sudo pacman -S gvfs-mtp fuse transmission-gtk mpv bash-completion openrc-bash-completions &&
 #paru
 git clone https://aur.archlinux.org/paru.git && cd paru/ && makepkg -si && cd &&
 #Other shit
